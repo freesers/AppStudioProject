0e64f1e141a09893097345e8f80aa19ca7b9f7a8
Sander de Vries <sanderdv@icloud.com>
2019-01-25 17:55:06 +0100

    beta1.1

---
+++ Sweep/Sweep/NotificationController.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationController.swift
+//  Sweep
+//
+//  Created by Sander de Vries on 25/01/2019.
+//  Copyright © 2019 Sander de Vries. All rights reserved.
+//
+
+import Foundation
